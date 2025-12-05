cbea2ba2ca114a87b81ea99c9690565e89c3e8f7
YoanMln <yoan.moulin@hotmail.fr>
2025-12-05 12:20:07 +0100

    Setup router

---
+++ Front-end/src/router/router.js
@@ -0,0 +1,56 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "@/views/HomeView.vue";
+import MoviesView from "@/view/MoviesView.vue";
+import MovieDetailView from "@/views/MovieDetailView.vue";
+import LoginView from "@/views/LoginView.vue";
+import RegisterView from "@/views/RegisterView.vue";
+import ProfileView from "@/views/ProfileView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASEURL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/movies",
+      name: "movies",
+      component: MoviesView,
+    },
+    {
+      path: "/movies/:id",
+      name: "/movie-detail",
+      component: MovieDetailView,
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: LoginView,
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: RegisterView,
+    },
+    {
+      path: "/profile/:id?",
+      name: "profile",
+      component: ProfileView,
+      meta: { requiresAuth: true },
+    },
+  ],
+});
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem("token");
+
+  if (to.meta.requiresAuth && !token) {
+    next({ name: "login" });
+  } else {
+    next();
+  }
+});
+
+export default router;
