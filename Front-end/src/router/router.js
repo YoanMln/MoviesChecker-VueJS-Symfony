299a19c46055df3c4201be7e80a99c252442e209
YoanMln <yoan.moulin@hotmail.fr>
2025-12-05 16:32:24 +0100

    Update router

--- Front-end/src/router/router.js
+++ Front-end/src/router/router.js
@@ -7,7 +7,7 @@ import RegisterView from "@/views/RegisterView.vue";
 import ProfileView from "@/views/ProfileView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASEURL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
